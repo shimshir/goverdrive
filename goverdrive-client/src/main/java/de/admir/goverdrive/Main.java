55fa91d0925279b827b2e7475cac59026bad70bd
admirmemic <admir.memic@springer.com>
2016-12-09 15:24:37 +0100

    Implemented file download, added a cache for certain API calls

--- goverdrive-client/src/main/java/de/admir/goverdrive/Main.java
+++ goverdrive-client/src/main/java/de/admir/goverdrive/Main.java
@@ -3,11 +3,22 @@ package de.admir.goverdrive;
 import de.admir.goverdrive.core.GoverdriveService;
 import de.admir.goverdrive.core.GoverdriveServiceImpl;
 
-public class Main {
-    private static final GoverdriveService gds = new GoverdriveServiceImpl();
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
-    public static void main(String[] args) {
-		 Object result = gds.createFile("/home/amemic/projects/goverdrive/goverdrive-core/src/main/java/de/admir/goverdrive/core/GoverdriveServiceImpl.java", "/GoverdriveServiceImpl.java");
-		 System.out.println(result);
-    }
+public class Main {
+
+   private static final GoverdriveService gds = new GoverdriveServiceImpl();
+
+   public static void main(String[] args) {
+
+      gds.getFileStream("/ch1.png").mapRight(outputStream -> {
+         try(OutputStream fileStream = new FileOutputStream("/home/amemic/ch1.png")) {
+            outputStream.writeTo(fileStream);
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+         return null;
+      });
+   }
 }
--- goverdrive-core/src/main/java/de/admir/goverdrive/core/GoverdriveService.java
+++ goverdrive-core/src/main/java/de/admir/goverdrive/core/GoverdriveService.java
@@ -1,26 +1,26 @@
 package de.admir.goverdrive.core;
 
-
 import com.google.api.services.drive.model.File;
 
 import de.admir.goverdrive.core.error.DriveError;
 import de.admir.goverdrive.core.util.Xor;
 
-import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 public interface GoverdriveService {
-    OutputStream getFileStream(String path);
 
-    Xor<DriveError, File> createFile(String localPath, String remotePath);
+   Xor<DriveError, ByteArrayOutputStream> getFileStream(String path);
 
-    Xor<DriveError, File> getRootFolder();
+   Xor<DriveError, File> createFile(String localPath, String remotePath);
 
-    Xor<DriveError, List<File>> getAllFilesAndFolders();
+   Xor<DriveError, File> getRootFolder();
 
-    Xor<DriveError, File> findFile(String remotePath);
+   Xor<DriveError, List<File>> getAllFilesAndFolders();
 
-    Xor<DriveError, List<File>> getFilePath(String remotePath);
+   Xor<DriveError, File> findFile(String remotePath);
 
-    Xor<DriveError, File> createFolder(String remotePath);
+   Xor<DriveError, List<File>> getFilePathList(String remotePath);
+
+   Xor<DriveError, File> createFolder(String remotePath, boolean createIntermediate);
 }
