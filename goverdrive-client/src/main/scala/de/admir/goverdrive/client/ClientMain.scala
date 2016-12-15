3092a40a2d4c9757d3db490a9e920be979707e06
shimshir <me.admir@gmail.com>
2016-12-15 17:21:10 +0100

    Minor changes

--- goverdrive-client/src/main/scala/de/admir/goverdrive/client/ClientMain.scala
+++ goverdrive-client/src/main/scala/de/admir/goverdrive/client/ClientMain.scala
@@ -1,5 +1,7 @@
 package de.admir.goverdrive.client
 
-object ClientMain extends App {
-    println("Hello, world!")
+object ClientMain {
+    def main(args: Array[String]): Unit = {
+        args foreach println
+    }
 }
